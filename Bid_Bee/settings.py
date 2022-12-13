eee501f142bc30e5a96ba0f85db9ca7da11fbcab
Nilesh0420 <n.ranjan1318@gmail.com>
2022-12-14 03:31:09 +0530

    customized commit message

--- Bid_Bee/settings.py
+++ Bid_Bee/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'eb1207a6-c557-4a94-b420-8dfff8689681'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 # Application references
 # https://docs.djangoproject.com/en/2.1/ref/settings/#std:setting-INSTALLED_APPS
